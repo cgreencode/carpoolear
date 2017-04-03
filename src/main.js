b7e16048a17ac990a0e74327013c9c4781bd3aa6
anproagile <anproagile@gmail.com>
2017-04-03 19:57:33 -0300

    Threads System

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 
+import VueAnalytics from 'vue-analytics'
+
 import router from './router'
 import store from './store'
 
 import cordova from './cordova'
+
+import font_awesome_css from './styles/font-awesome/css/font-awesome.min.css'
+
+import bootstrap_css from './styles/bootstrap/css/bootstrap.min.css'
 
 import css from './styles/main.css'
 
@@ -17,7 +23,18 @@ let app = new Vue({
   components: { App }
 })
 
+ 
+Vue.use(VueAnalytics, {
+  id: 'UA-40995702-4'
+});
+
 /* Just for debuggin */
 window.Vue = Vue
 window.store = store
+
+import { Thread, stopThreads }  from './classes/Threads'
+window.Thread = Thread
+window.stopThreads = stopThreads
+
+
 
