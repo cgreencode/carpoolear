0b9331c673fe55877a4fd6c79666c1398f0025a7
anproagile <anproagile@gmail.com>
2017-08-05 08:21:01 -0300

    register requirements, style corrections, formatter correction

--- src/components/views/Trip.vue
+++ src/components/views/Trip.vue
@@ -56,13 +56,13 @@
                                         <span class="trip_datetime_time">{{ [ trip.trip_date ] | moment("h:mm a") }}</span>
                                     </time>
                                 </div>
-                                <div class="row"  v-if="!trip.is_passenger">
+                                <div class="row">
                                     <div class="trip_seats-available col-xs-offset-4 col-sm-offset-4 col-xs-12">
                                         <span class="trip_seats-available_value pull-left">{{ trip.seats_available }}</span>
                                         <span class="trip_seats-available_label">Lugares<br>libres</span>
                                     </div>
                                 </div>
-                                <div class="row passengers"  v-if="!trip.is_passenger">
+                                <div class="row passengers">
                                     <div class="col-xs-offset-3 col-xs-21" v-if="trip.passenger.length">
                                         <span v-for="p in trip.passenger">
                                             <div class="trip_driver_img circle-box passenger" v-imgSrc:profile="trip.passenger.image">
@@ -76,25 +76,11 @@
                                 </div>
                             </div>
                             <div class="col-sm-10 col-md-10 column">
-                                <div class="row trip-data" v-if="trip.is_passenger">
-                                    <strong class="warning-is-passenger">Pasajero que busca viaje</strong>
-                                </div>
                                 <div class="row trip-data">
-                                    <em v-if="trip.friendship_type_id == 2">
-                                        <i class="fa fa-globe" aria-hidden="true"></i>
-                                        Viaje público
-                                    </em>
-                                    <em v-if="trip.friendship_type_id == 1">
-                                        <i class="fa fa-users" aria-hidden="true"></i>
-                                        Amigos de amigos
-                                    </em>
-                                    <em v-if="trip.friendship_type_id == 0">
-                                        <i class="fa fa-user" aria-hidden="true"></i>
-                                        Sólo amigos
-                                    </em>
+                                    <span>Viaje Público</span><br>
+                                    <span>Gastos compartidos</span>
                                 </div>
-
-                                <div class="row trip-stats"  v-if="!trip.is_passenger">
+                                <div class="row trip-stats">
                                     <div>
                                         <span>Distancia a recorrer</span><br>
                                         <span>{{ distanceString }} <abbr title="kilometros">km</abbr></span>
@@ -125,10 +111,8 @@
                             </div>
                             <div class="buttons-container">
                                 <router-link class="btn btn-primary" v-if="owner" :to="{name: 'update-trip', params: { id: trip.id}}"> Editar  </router-link>
-                                <template v-if="!owner && !expired">
-                                    <button class="btn btn-primary" @click="toMessages" v-if="!owner"> Coordinar viaje  </button>
-                                </template>
                                 <template v-if="!owner && !trip.is_passenger && !expired">
+                                    <button class="btn btn-primary" @click="toMessages" v-if="!owner"> Coordinar viaje  </button>
                                     <template v-if="!isPassenger">
                                         <button class="btn btn-primary" @click="makeRequest" v-if="canRequest"> Solicitar asiento </button>
                                         <button class="btn" v-if="!canRequest" @click="cancelRequest"> Solicitud enviada </button>
@@ -155,9 +139,6 @@
                                     <div class="profile-info--ratings">
                                         <svgItem icon="thumbUp" size="18"></svgItem> <span> {{trip.user.positive_ratings}} </span>
                                         <svgItem icon="thumbDown" size="18"></svgItem> <span> {{trip.user.negative_ratings}} </span>
-                                    </div>
-                                    <div v-if="trip.user.has_pin == 1" class="user_pin">
-                                        <img src="https://carpoolear.com.ar/static/img/pin.png" alt="" title="Aportante en la campaña mi media naranja carpoolera" />
                                     </div>
                                 </div>
                             </div>
@@ -240,7 +221,13 @@ export default {
             make: 'passenger/makeRequest',
             cancel: 'passenger/cancel'
         }),
-
+        profileComplete () {
+            if (!this.user.image || this.user.image.length === 0 || !this.user.description || this.user.description.length === 0) {
+                router.replace({ name: 'profile_update' });
+            } else {
+                return true;
+            }
+        },
         loadTrip () {
             this.getTrip(this.id).then(trip => {
                 // this.trip = trip;
@@ -257,19 +244,23 @@ export default {
         },
 
         toMessages () {
-            this.lookConversation(this.trip.user).then(conversation => {
-                router.push({ name: 'conversation-chat', params: { id: conversation.id } });
-            });
+            if (this.profileComplete()) {
+                this.lookConversation(this.trip.user).then(conversation => {
+                    router.push({ name: 'conversation-chat', params: { id: conversation.id } });
+                });
+            }
         },
 
         makeRequest () {
-            this.sending = true;
-            this.make(this.trip.id).then(() => {
-                this.sending = false;
-                this.trip.request = 'send';
-            }).catch(() => {
-                this.sending = false;
-            });
+            if (this.profileComplete()) {
+                this.sending = true;
+                this.make(this.trip.id).then(() => {
+                    this.sending = false;
+                    this.trip.request = 'send';
+                }).catch(() => {
+                    this.sending = false;
+                });
+            }
         },
 
         cancelRequest () {
@@ -431,12 +422,6 @@ export default {
 </script>
 
 <style scoped>
-    .user_pin {
-        margin-top: 1em;
-    }
-    .user_pin img {
-        width: 40px;
-    }
     .trip_driver_img.circle-box.passenger {
         width: 2.2em;
         height: 2.2em;
