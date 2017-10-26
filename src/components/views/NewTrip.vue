301800cd20cc55380bfd563d254ac90a73c2e437
anproagile <anproagile@gmail.com>
2017-10-26 14:41:46 -0300

    Modified links ending with .php

--- src/components/views/NewTrip.vue
+++ src/components/views/NewTrip.vue
@@ -119,7 +119,7 @@ Tengamos un buen viaje cuidándonos entre todos :D">
                                 </ul>
                             </fieldset>
 
-                            <button class="trip-create btn btn-primary btn-lg btn-shadowed" @click="save" :disabled="saving">
+                            <button class="trip-create btn btn-primary btn-lg btn-shadowed" @click="save">
                                 <span v-if="!updatingTrip">CREAR</span>
                                 <span v-else>Actualizar</span>
                             </button>
@@ -228,8 +228,7 @@ export default {
                 'enc_path': '123',
                 'points': [] /* address json_address lat lng */
             },
-            updatingTrip: null,
-            saving: false
+            updatingTrip: null
         };
     },
     mounted () {
@@ -408,7 +407,6 @@ export default {
                 return;
             }
             /* eslint-disable no-unreachable */
-            this.saving = true;
             this.trip.points = [];
             this.points.forEach(p => {
                 let point = {};
@@ -430,16 +428,14 @@ export default {
             }
             if (!this.updatingTrip) {
                 this.createTrip(this.trip).then((t) => {
-                    this.saving = false;
                     this.$router.replace({ name: 'detail_trip', params: { id: t.id } });
-                }).catch(() => { this.saving = false });
+                });
             } else {
                 console.log(this.trip);
                 this.trip.id = this.updatingTrip.id;
                 this.updateTrip(this.trip).then(() => {
-                    this.saving = false;
                     this.$router.replace({ name: 'detail_trip', params: { id: this.trip.id } });
-                }).catch(() => { this.saving = false });
+                });
             }
         },
 
