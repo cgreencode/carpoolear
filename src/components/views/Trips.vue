c4ab40a6c0ba59cc4e425e1ee7091f13260f38cb
anproagile <anproagile@gmail.com>
2019-11-12 12:04:17 -0300

    desktop header correction

--- src/components/views/Trips.vue
+++ src/components/views/Trips.vue
@@ -77,16 +77,11 @@
                 {{ $t('cargandoMasResultados') }}
             </div>
             <p slot="no-data" class="alert alert-warning"  role="alert"  :class="isMobile ? 'mobile-alert' : ''">
-                <template v-if="filtered">
-                    <span class="sentence">{{ $t('noHayViajes') }}</span>
-                    <span class="sentence" v-if="!alreadySubscribe">
-                        <strong :class="isMobile ? 'sentence' : ''"> {{ $t('subscribirteAViajes') }}</strong>
-                        <button class="btn btn-primary" v-if="user" @click="subscribeSearch" >{{ $t('crearAlerta') }}</button>
-                    </span>
-                </template>
-                <template v-else>
-                    <span class="sentence">{{ $t('noHayViajesCargadosAun') }}</span>
-                </template>
+                <span class="sentence">{{ $t('noHayViajes') }}</span>
+                <span class="sentence" v-if="!alreadySubscribe">
+                    <strong :class="isMobile ? 'sentence' : ''"> {{ $t('subscribirteAViajes') }}</strong>
+                    <button class="btn btn-primary" v-if="user" @click="subscribeSearch" >{{ $t('crearAlerta') }}</button>
+                </span>
             </p>
             <p slot="loading" class="alert alert-info" role="alert">
                 <img src="https://carpoolear.com.ar/static/img/loader.gif" alt="" class="ajax-loader" />
--- src/store/modules/actionbars.js
+++ src/store/modules/actionbars.js
@@ -1,12 +1,9 @@
 import * as types from '../mutation-types';
 import router from '../../router';
 import globalStore from '../index';
-let appName = process.env.TARGET_APP;
-if (appName && appName.length) {
-    appName = appName.charAt(0).toUpperCase() + appName.slice(1);
-}
+
 const state = {
-    title: appName,
+    title: 'Carpoolear',
     subTitle: '',
     imgTitle: '',
     showMenu: false,
@@ -86,16 +83,10 @@ const getters = {
 };
 
 const actions = {
-    setTitle (store, title = '') {
-        let getters = globalStore.getters;
-        let config = getters['auth/appConfig'];
-        let appName = config ? config.app_name : process.env.TARGET_APP;
-        if (appName && appName.length) {
-            appName = appName.charAt(0).toUpperCase() + appName.slice(1);
-        }
+    setTitle (store, title = 'Carpoolear') {
         store.commit(types.HEADER_SET_TITLE, title);
         if (document) {
-            document.title = title + (title !== appName ? (' - ' + appName) : '');
+            document.title = title + (title !== 'Carpoolear' ? ' - Carpoolear' : '');
         }
     },
 
